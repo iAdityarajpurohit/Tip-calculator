f484ead742ec71d58282f61f8033496feaae7f8b
iAdityarajpurohit <108542528+iAdityarajpurohit@users.noreply.github.com>
2022-11-25 13:32:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 
-var string = 0;
+var str = 0;
 function addper(input){
     document.getElementById("custom").value = input
-    string  = input;
+    str  = input;
 }
 
 const tipCalcy = ()=> {
     
     let custom = document.getElementById("custom").value;
-    console.log(string + " string is ");
+    // console.log(str + " str is ");
    var bill = document.getElementById("bill").value;
    var people = document.getElementById("people").value;
-   if(string == 0){
+   if(str == 0){
         // console.log(custom + ' value check');
-        string = custom;
-        var total = (bill*string)/100;
+        str = custom;
+        var total = (bill*str)/100;
         console.log(total);
-        var perPerson = total/people
-       
-   }
-   else{
-       var total = (bill*string)/100;
-       var perPerson = total/people
+        var perPerson =(total/people);
+        perPerson = parseFloat(perPerson);
+        console.log(typeof(perPerson), " typeof");
+        
+    }
+    else{
+        var total = (bill*str)/100;
+        var perPerson = (total/people)
+        perPerson = parseFloat(perPerson);
     
     }
  
@@ -37,13 +40,15 @@ const tipCalcy = ()=> {
  var bill =  parseInt(bill)
  var total =  parseInt(total)
  var bilTip = bill+total;
+ 
 
-const subT=(bill/people)+perPerson
+const subT=(((bill/people)+perPerson)).toFixed(2)
+// console.log(typeof(subT), " subT");
 
    tip.innerHTML = total;
    perMember.innerHTML = perPerson;  
    total=0;
-    string = 0;
+    str = 0;
     custom=0;
     tip1.innerHTML = bilTip;
    perMember1.innerHTML = subT ; 
@@ -60,7 +65,7 @@ reset.addEventListener('click',()=>{
     people.value = "";
    tip.innerHTML = 0.00;
    perMember.innerHTML = 0.00;  
-   string = 0;
+   str = 0;
    tip1.innerHTML = 0;
    perMember1.innerHTML =0 ;
 });
